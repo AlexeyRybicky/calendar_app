dd6868c266c3aaec9e99b7ff541a7a8fc6cc93b4
Aleksey <ranangariel@gmail.com>
2024-08-30 18:34:44 +0300

    изменил модели БД

--- backend/simple_calendar/models.py
+++ backend/simple_calendar/models.py
@@ -1,7 +1,6 @@
 """В моделу содержиться модель базы данных приложения `simple_calendar`"""
 
 from django.db import models
-from django.utils import timezone
 
 
 class Event(models.Model):
@@ -16,38 +15,42 @@ class Event(models.Model):
     start_time = models.DateTimeField(
         help_text="Дата начанала события"
     )
-    recurrence = models.IntegerField(
-        null=True,
-        blank=True,
-        help_text="Периодичность повторения"
-    )
 
     def __str__(self):
         """Возвращает имя события"""
         return self.name
 
 
-class RecurringEvent(models.Model):
+class Recurrence(models.Model):
     """Модель для повторяющихся событий"""
 
     objects: models.Manager
 
-    event = models.ForeignKey(
+    # class Frequency(models.TextChoices):
+    #     DAILY = 'daily', 'Daily'
+    #     WEEKLY = 'weekly', 'Weekly'
+    #     MONTHLY = 'monthly', 'Monthly'
+
+    event = models.OneToOneField(
         Event,
         on_delete=models.CASCADE,
-        related_name='recurring_events'
+        related_name='recurrence'
     )
-    recurrence_date = models.DateField(
-        help_text='Дата повторения'
+    # frequency = models.CharField(
+    #     max_length=50,
+    #     choices=Frequency.choices,
+    #     help_text="Частота повторения"
+    # )
+    interval = models.PositiveIntegerField(
+        default=1,
+        help_text="Интервал повторения"
     )
-    is_cancelled = models.BooleanField(
-        default=False,
-        help_text='Фалг отмены события'
+    until = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="Дата окончания повторений"
     )
 
     def __str__(self):
-        """Возвращает даты повторения у события"""
-        return f"{self.event.name} - {self.recurrence_date}"
-
-    class Meta:
-        unique_together = ('event', 'recurrence_date')
+        """Возвращает параметры повторения"""
+        return f"{self.event.name} - повторяется каждые {self.interval}"
