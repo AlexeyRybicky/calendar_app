48f25e4b1765d6be1c41e451721b5942c47a3408
Aleksey <ranangariel@gmail.com>
2024-08-30 19:13:44 +0300

    изменил эндпоинт remove_event, update_event, get_events

--- backend/simple_calendar/views.py
+++ backend/simple_calendar/views.py
@@ -4,33 +4,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from simple_calendar.models import Event, Recurrence
-
-
-# @api_view(['POST'])
-# def add_event(request):
-#     """
-#     Добавляет событие
-#     """
-#
-#     data = request.data
-#     event = Event(
-#         name=data['name'],
-#         start_time=datetime.strptime(data['start_time'], '%Y-%m-%dT%H:%M:%S'),
-#         recurrence=data.get('recurrence')
-#     )
-#     event.save()
-#
-#     # Если указана периодичность, то создаем последовательность событий на год вперед
-#     if event.recurrence:
-#         recurrence_date = event.start_time.date()
-#         while recurrence_date <= datetime.now().date() + timedelta(days=365):
-#             RecurringEvent(event=event, recurrence_date=recurrence_date).save()
-#             recurrence_date += timedelta(days=event.recurrence)
-#
-#     return Response({'id': event.id}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
@@ -45,7 +21,7 @@ def add_event(request):
         start_time=datetime.strptime(data['start_time'], '%Y-%m-%dT%H:%M:%S'),
     )
 
-    # Если указана периодичность, то создаем последовательность событий
+    # Если указана периодичность, то задаем правила повторения
     if 'period' in data and data['period'] is not None:
         Recurrence.objects.create(
             event=event,
@@ -59,19 +35,27 @@ def add_event(request):
 @api_view(['POST'])
 def remove_event(request, id, year, month, day):
     """
-    Удаляет событие по id, оставляя все последующие
+    Удаляет конкретное событие по id, оставляя все последующие.
     """
 
     event_id = int(id)
-    date = datetime(int(year), int(month), int(day)).date()
+    date = datetime(int(year), int(month), int(day))
 
     event = get_object_or_404(Event, id=event_id)
-    deleted_count, _ = RecurringEvent.objects.filter(event=event, recurrence_date=date).delete()
 
-    return Response({'message': 'Событие удалено'}) if deleted_count > 0 else Response(
-        {'message': 'Повторяющееся событие с указанной датой не найдено'},
-        status=status.HTTP_404_NOT_FOUND
-    )
+    if hasattr(event, 'recurrence'):
+        recurrence = event.recurrence
+        current_event_start_time = event.start_time
+
+        # Если событие повторяется, удаляет текущее событие
+        if recurrence.until and current_event_start_time > recurrence.until:
+            return Response({'message': 'Событие уже закончилось'}, status=status.HTTP_400_BAD_REQUEST)
+
+        event.delete()
+        return Response({'message': 'Текущее событие удалено'})
+
+    event.delete()
+    return Response({'message': 'Событие удалено'})
 
 
 @api_view(['POST'])
@@ -84,7 +68,7 @@ def remove_next_events(request, id, year, month, day):
     date = datetime(int(year), int(month), int(day)).date()
 
     event = get_object_or_404(Event, id=event_id)
-    deleted_count, _ = RecurringEvent.objects.filter(event=event, recurrence_date__gte=date).delete()
+    deleted_count, _ = Recurrence.objects.filter(event=event, recurrence_date__gte=date).delete()
 
     return Response({'message': 'Все последующие события удалены'}) if deleted_count > 0 else Response(
         {'message': 'Не найдено событий для удаления'},
@@ -100,7 +84,7 @@ def update_event(request, id, year, month, day):
 
     event_id = int(id)
     date = datetime(int(year), int(month), int(day)).date()
-    recurring_event = RecurringEvent.objects.get(event_id=event_id, recurrence_date=date)
+    recurring_event = Recurrence.objects.get(event_id=event_id)
     recurring_event.event.name = request.data['name']
     recurring_event.event.save()
     return Response({'message': 'Событие обновлено'})
@@ -113,6 +97,7 @@ def get_events(request, year, month, day):
     """
 
     date = datetime(int(year), int(month), int(day)).date()
-    events = RecurringEvent.objects.filter(recurrence_date=date).select_related('event')
-    result = [{'id': event.event.id, 'name': event.event.name} for event in events if not event.is_cancelled]
+    events = Event.objects.filter(start_time__date=date).select_related('recurrence')
+    result = [{'id': event.id, 'name': event.name} for event in events]
+
     return Response(result)
